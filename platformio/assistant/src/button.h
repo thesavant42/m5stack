a27cb23d50b2474e12060784e28e7847325b348b
himanushi <himanushi@users.noreply.github.com>
2024-02-09 20:35:37 +0900

    button 追加対応

---
+++ platformio/assistant/src/button.h
@@ -0,0 +1,67 @@
+#include <M5Unified.h>
+
+#include "efont.h"
+#include "efontM5Unified.h"
+#include "efontEnableJaMini.h"
+
+static constexpr const char* state_name[16] = {
+            "none", "touch", "touch_end", "touch_begin",
+            "___",  "hold",  "hold_end",  "hold_begin",
+            "___",  "flick", "flick_end", "flick_begin",
+            "___",  "drag",  "drag_end",  "drag_begin"};
+
+// ボタンがタッチされたときに実行する関数の型を定義
+typedef void (*ButtonAction)();
+
+class Button {
+public:
+    int x, y, width, height;
+    String label;
+    ButtonAction action;
+
+    Button(int x, int y, int width, int height, String label, ButtonAction action)
+    : x(x), y(y), width(width), height(height), label(label), action(action) {}
+
+    // ボタンをLCDに描画
+    void draw() {
+        M5.Lcd.drawRect(x, y, width, height, WHITE);
+        printEfont(const_cast<char*>(label.c_str()), 0, x + 5, y + height / 4);
+    }
+
+    // 指定された座標がボタン内か判定
+    bool contains(int tx, int ty) {
+        return (tx >= x && tx <= x + width && ty >= y && ty <= y + height);
+    }
+
+    // ボタンがタッチされたときのアクションを実行
+    void press() {
+        if (action) action();
+    }
+};
+
+
+#include <vector>
+
+class ButtonManager {
+public:
+    std::vector<Button> buttons;
+
+    void addButton(const Button& button) {
+        buttons.push_back(button);
+    }
+
+    void drawButtons() {
+        for (auto& button : buttons) {
+            button.draw();
+        }
+    }
+
+    void handleTouch(int x, int y) {
+        for (auto& button : buttons) {
+            if (button.contains(x, y)) {
+                button.press();
+                break;
+            }
+        }
+    }
+};
--- platformio/assistant/src/main.cpp
+++ platformio/assistant/src/main.cpp
@@ -3,15 +3,37 @@
 #include "efont.h"
 #include "efontM5Unified.h"
 #include "efontEnableJaMini.h"
+#include "button.h"
+
+ButtonManager buttonManager;
+
+void actionButton1() {
+  Serial.println("Button 1 pressed");
+}
+
+void actionButton2() {
+  Serial.println("Button 2 pressed");
+}
+
+Button button1(0, 140, 160, 100, "何か話して", actionButton1);
+Button button2(160, 140, 160, 100, "録音", actionButton2);
 
 void setup() {
   M5.begin();
-  
-  printEfont("Hello", 1, 0, 16*0);
-  printEfont("こんにちは", 2, 0, 16*1);
-  printEfont("1月４日　土曜日　９時５５分", 3, 0, 16*3);
+  M5.Lcd.fillScreen(BLACK);
+
+  Serial.begin(115200);
+
+  buttonManager.addButton(button1);
+  buttonManager.addButton(button2);
+  buttonManager.drawButtons();
 }
 
 void loop() {
   M5.update();
+
+  auto detail = M5.Touch.getDetail();
+  if (state_name[detail.state] == "touch_begin") {
+    buttonManager.handleTouch(detail.x, detail.y);
+  }
 }
